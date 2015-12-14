db1ccadf8cb59841b4c5e6e38df4692260a577c0
ioan <ioan.agavriloaei@gmail.com>
2015-12-14 17:36:04 +0200

    PostCSS + SASS

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,17 @@
 
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
 
 var atImport = require('postcss-import');
 var mqpacker = require('css-mqpacker');
+var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
 
 var precss = require('precss');
 
 gulp.task('css', function () {
-    var processors = [
+    /*var processors = [
         atImport,
         mqpacker,
         cssnano({
@@ -22,9 +24,18 @@ gulp.task('css', function () {
 
     var processors = [
         precss
+    ];*/
+
+    var processors = [
+        autoprefixer,
+        cssnano
     ];
 
-    return gulp.src('./src/*.css')
+    /*return gulp.src('./src/!*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dest'));*/
+    return gulp.src('./src/*.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(postcss(processors))
         .pipe(gulp.dest('./dest'));
 });
